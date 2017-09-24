dbb7e2e5dc3146a673603490ac9f2e75f6da931b
Manzeon <820380626@qq.com>
2017-09-24 12:49:05 +0800

    learning

---
+++ es6/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "parser": "babel-eslint",
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
